dc88259e5ea394eac28352954cf6f7e4a795d317
oscargiann <130966440+oscargiann@users.noreply.github.com>
2023-04-17 09:23:35 +0200

    Add files via upload

---
+++ test_sum.py
@@ -0,0 +1,16 @@
+from sums.sum_numbers import sum_two, operation
+from pytest import raises
+import xarray as xr
+import numpy as np
+import pandas as pd
+
+def test_sum():
+    a = 10
+    b = 1
+    c = sum_two(a, b)
+
+    assert c == a + b
+
+
+def test_operation():
+    assert isinstance(operation(), xr.DataArray)
